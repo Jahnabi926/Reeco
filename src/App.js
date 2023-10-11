ebc6591f689a55c821722b0988f5abf59d15d7aa
unknown <sarmajahnabi8@gmail.com>
2023-10-11 19:23:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from "react";
+import mockData from "./MOCK_DATA.json";
 import Table from "./components/Table";
+import Navbar from "./components/Navbar";
+import styled from "styled-components";
 
+const IMG = styled.img`
+width: 50px;
+backgroundColor: rgba(255, 255, 255, 0.2);`
+
+console.log(mockData);
 const App = () => {
-  const data = React.useMemo(
-    () => [
-      { id: 1, name: "John Doe", age: 28 },
-      { id: 2, name: "Jane Smith", age: 32 },
-    ],
-    []
-  );
+  const data = React.useMemo(() => mockData, []);
   const columns = React.useMemo(
     () => [
-      { Header: "ID", accessor: "id" },
-      { Header: "Name", accessor: "name" },
-      { Header: "Age", accessor: "age" },
+      { Header: "", accessor: "icon", Cell: ({cell: {value}}) => (
+        <div><IMG src={require("./images/" + value)} alt={value}/></div>
+      ) },
+      { Header: "Product name", accessor: "product_name" },
+      { Header: "Brand", accessor: "brand" },
+      { Header: "Price", accessor: "price" },
+      { Header: "Quantity", accessor: "quantity" },
+      { Header: "Total", accessor: "total" },
+      { Header: "Status", accessor: "status" },
     ],
     []
   );
   return (
-    <div>
-      <h1>React Table Example</h1>
+    <div className="container">
+      <Navbar />
       <Table columns={columns} data={data} />
     </div>
   );
--- src/components/Table.js
+++ src/components/Table.js
@@ -11,7 +11,6 @@ const Table = ({ columns, data }) => {
       <thead>
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
-            |
             {headerGroup.headers.map((column) => (
               <th {...column.getHeaderProps()}>{column.render("Header")}</th>
             ))}
